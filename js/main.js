aaf8c71d379a431ccd1128df9c8bd141e1720f0f
kelly luo <kellyluo25@gmail.com>
2020-08-26 18:03:05 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,9 +7,8 @@ var oceancontent = [
     {text: "And I hear your ship is comin' in <br /> Your tears a sea for me to swim <br /> And I hear a storm is comin' in <br /> My dear, is it all we’ve ever been?", title: "Anchor", attr: "Novo Amor"},
     {text: "If you're gonna let me down, let me down gently <br /> Don't pretend that you don't want me <br /> Our love ain't water under the bridge", title: "Water Under the Bridge", attr: "Adele"},
     {text: "And the arms of the ocean are carrying me <br /> And all this devotion was rushing out of me  <br /> And the crashes are heaven, for a sinner like me  <br /> But the arms of the ocean delivered me", title: "Never Let Me Go", attr: "Florence + the Machine"},
-    {text: "you broke the ocean in <br /> half to be here.<br />only to meet nothing that wants you.", title: "immigrant", attr: "Nayyirah Waheed"}
-  {text: "for whatever we lose <br /> (like a you or a me), <br/> it's always ourselves we find in the sea.", title: "maggie and milly and molly and may", attr: "e.e. cummings"}
-
+    {text: "you broke the ocean in <br /> half to be here.<br />only to meet nothing that wants you.", title: "immigrant", attr: "Nayyirah Waheed"},
+  	{text: "for whatever we lose <br /> (like a you or a me), <br/> it's always ourselves we find in the sea.", title: "maggie and milly and molly and may", attr: "e.e. cummings"}
     
 ];
 
